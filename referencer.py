3d6475fe13b423c65d4a3fbb14eebf22c9f4fbb0
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2020-01-19 18:03:57 +0930

    changed code to run console prompts so user can dynamically add names to list, fixed comments, formatting

--- referencer.py
+++ referencer.py
@@ -9,39 +9,41 @@ import webbrowser
 # author information is in "first name, last name" format. This is just a quick thingy to then be able to paste straight
 # into endnote. Probably not very useful to most people at all....but you know....coding is life. 
 
-## function to store names into a text file (same one this python file is in)
+# Function to store names into a text file (same one this python file is in)
 def store_name(author):
     """ Store name in a file """
     with open("ref.txt","a") as fp:
         fp.write(str(reference)+""+'\n')
- 
-names = (['Meaghan Altman','joshua vanArsdall', 'Brett Beston', 'Kwame Brown','Angela Lukowski',
-'Emily Slonecker','John Hummel','Hillary Wehe','John Vervaeke','Melissa Swisher','Bernardo Carducci',
-'Thomaseo Burton','Stephanie Poplock'])
 
-#can also have as dynamic input from user just uncomment code snippet below 
-#(actually this isn't finished at the moment, let me know if you think this code would be useful for you and I'll finish it up)
+# Loop that provides console prompts to the user so that they can enter the names themselves
+x = 0 
+names = []
+while x == 0:
 
-# def dynamic_Names():
-#     Fnames = input("Fname? ")
-#     Lnames = input("Lname? ")
-#     input ("Add more Y/N ? "):
-#         if input == "Y"
-#     names = [Fnames+" "+Lnames]
+    Fnames = input("First name? ")
+    Lnames = input("Last name? ")
+    names.append(Fnames+" "+Lnames)    
+    add_name = input ("Do you want to add more Y/N ? ")
+
+    if add_name  == "Y"or add_name == "y":
+      
+        print (names)
+
+    elif add_name  == "N" or add_name == "n":
+        
+        print (names)
+        x = 1
+        
 #################################################################################################
 
 for i in names:
     bro=i.split()
     bro.reverse()
     reference = (', '.join(bro))
-
-    # print (reference)
-
     store_name(reference)
 
 # just a quick console prompt to check whether you want to open the txt file where the names are stored
 open_file = input ("Do you wanna to open the file now? Y/N ")
-
 if open_file == "y" or open_file == "Y" or open_file == "yes" or open_file == "Yes": 
     print (open_file)
     webbrowser.open('ref.txt')
